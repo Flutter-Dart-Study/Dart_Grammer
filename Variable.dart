22f9a4b80dc503e2e2cfc26abfd0075ab11eb48c
RosieOh <20172207@gm.hannam.ac.kr>
2023-12-15 03:16:08 +0900

    🐦feat : 변수 선언 실습🐦

---
+++ Variable.dart
@@ -0,0 +1,25 @@
+void main() {
+    /// 변수란 ? - 변할 수 있는 수 (값)
+    String name = '오레오'; // 오레오 라는 문자열을
+                    // name이라는 변수에 값을 할당한다.
+    print(name);
+
+    int age = 30;
+    print(age);
+
+    var name2 = "홍길동"; // dart에서 지원하는 문법
+                         // 변수 값에 대한 타입 추론을 하게 됩니다.
+    print(name2);
+
+    var age2 = 30;
+    pring(age2);
+
+    // bool - true나 false의 논리를 정의할 때 사용함.
+    bool isChecked = false;
+    print(isChecked);
+
+    // double - 소수점 값을 표현하는 단위
+    double tall = 164.9;
+    print(tall);
+
+}
